dff34b2ff08007ba0d5cc6c82795a7a5c8b77187
Dave Clark <dave@dkcwd.com.au>
2013-10-21 21:28:13 +1100

    Fixed typo in comment

--- config/module.config.php
+++ config/module.config.php
@@ -2,7 +2,7 @@
 /**
  * ZF-Hipsters Abstract Factory (https://github.com/zf-hipsters)
  *
- * @link      https://github.com/zf-hipsters/zfh-abstract-factory for the canonical source repository
+ * @link      https://github.com/zf-hipsters/abstract-factory for the canonical source repository
  * @copyright Copyright (c) 2013 ZF-Hipsters
  * @license   http://www.apache.org/licenses/LICENSE-2.0 Apache Licence, Version 2.0
  */
